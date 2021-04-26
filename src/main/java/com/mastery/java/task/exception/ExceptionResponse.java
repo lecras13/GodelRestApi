cdbd8598bb23b4a2c3a765da10790763897842d6
lecras@tut.by <128vitrumx>
2021-04-26 15:40:07 +0300

    log4j2, slf4j logback, slf4j

---
+++ src/main/java/com/mastery/java/task/exception/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.mastery.java.task.exception;
+
+public class ExceptionResponse {
+    private String exceptionMessage;
+
+    public ExceptionResponse() {
+
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+
+    public void setExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+}
---
+++ src/main/java/com/mastery/java/task/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.mastery.java.task.exception;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setExceptionMessage(exception.getMessage());
+        logger.warn("Exception message: {}", exception.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionResponse> handleException(EmployeeNotFoundException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setExceptionMessage(exception.getMessage());
+        logger.warn("Example log from {}", exception.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
